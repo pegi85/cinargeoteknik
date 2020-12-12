4e280a33749ddfc6f9eac7d15be96a372405735d
pegi85 <pegi.arch@gmail.com>
2020-12-12 16:17:38 +0300

    form complete

---
+++ action-job.php
@@ -0,0 +1,44 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// Include PHPMailer library files
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+
+$mail = new PHPMailer;
+
+// SMTP configuration
+$mail->isSMTP();
+$mail->Host     = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'pegi.arch@gmail.com';
+$mail->Password = '83338610';
+$mail->SMTPSecure = 'tls';
+$mail->Port     = 587;
+
+$mail->setFrom('pegi.arch@gmail.com', $_POST['company']);
+
+// Add a recipient
+$mail->addAddress('b.naderalvojoud@gmail.com');
+
+// Email subject
+$mail->Subject = 'Web Siteden Gelen Proje Formu';
+
+// Set email format to HTML
+$mail->isHTML(true);
+
+// Email body content
+$mailContent = $_POST['message'];
+$mail->Body = $mailContent;
+
+// Send email
+if(!$mail->send()){
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+}else{
+    echo 'Message has been sent';
+}
+
+?>

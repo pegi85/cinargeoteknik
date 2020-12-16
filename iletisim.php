da053be8cfed46c74e0993151d0f90b6ce7cf9db
pegi85 <pegi.arch@gmail.com>
2020-12-16 21:07:26 +0300

    ready to go up

--- iletisim.php
+++ iletisim.php
@@ -4,7 +4,7 @@
 <html lang="en">
 
 <head>
-    <title>ÇINAR GEOTEKNIK</title>
+    <title>İLETIŞİM-ÇINAR GEOTEKNIK | Geoteknik Rapor | Fore Kazık | cinargeoteknik.com</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
